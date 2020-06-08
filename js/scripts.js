4610bd59c6566091e074d2256118fe2bcc1cd656
Emma Hellman <ehh801@icloud.com>
2020-06-08 16:10:24 -0700

    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,12 @@ $(document).ready(function() {
     const sentence = $("input#sentence").val();
     const splitsentence = sentence.split(' ');
     console.log(splitsentence);
+    event.preventDefault();
+    let newArray = []
+    splitsentence.forEach(function(word) {
+      newArray.push(word); 
+      console.log(newArray);   
+      ;  
+  });
   });
 });
